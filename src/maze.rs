5b20bd60078f331d11b118fd62a99191841b11d5
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-02-17 16:25:04 +0900

    feat: 🎸 change maze struct and added test

--- src/maze.rs
+++ src/maze.rs
@@ -1,19 +1,23 @@
 /*
  * 迷路の種類
  */
+use std::collections::BTreeMap;
+
+#[derive(PartialEq, Debug)]
 pub enum MazeKind {
     Path,
     Wall,
     Start,
-    End
+    End,
 }
 
 /*
  * 迷路の状態（見える見えない）
  */
-enum MazeFlag {
+#[derive(PartialEq, Debug)]
+pub enum MazeFlag {
     Visited,
-    Hide
+    Hide,
 }
 
 /*
@@ -23,13 +27,67 @@ enum MazeDirection {
     Up,
     Down,
     Left,
-    Right
+    Right,
+}
+
+pub type MazeKindAnnotation = BTreeMap<String, MazeKind>;
+
+#[derive(PartialEq, Debug)]
+pub struct MazeCell {
+    pub(crate) kind: MazeKind,
+    pub(crate) flag: MazeFlag,
+}
+
+pub struct MazeRow {
+    cells: Vec<MazeCell>,
 }
 
 pub struct Maze {
-    kind: MazeKind,
-    flag: MazeFlag
+    width: usize,
+    height: usize,
+    rows: Vec<MazeRow>,
 }
 
-const MAZE_WIDTH: u8 = 5;
-const MAZE_HEIGHT: u8 = 5;
+impl MazeCell {
+    fn create(kind: MazeKind, flag: MazeFlag) -> Self {
+        Self {
+            kind,
+            flag,
+        }
+    }
+
+    pub fn compare(a: &MazeKind, b: &MazeKind) -> bool {
+        a == b
+    }
+}
+
+impl MazeRow {
+    fn create(cells: Vec<MazeCell>) -> Self {
+        Self { cells }
+    }
+}
+
+impl Maze {
+    fn init(width: usize, height: usize, rows: Vec<MazeRow>) -> Self {
+        Self {
+            width,
+            height,
+            rows,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::maze::{MazeCell, MazeFlag, MazeKind};
+
+    #[test]
+    fn should_create_maze_cell() {
+        let kind = MazeKind::Start;
+        let flag = MazeFlag::Visited;
+        assert_eq!(&MazeCell::create(kind, flag), &MazeCell {
+            kind: MazeKind::Start,
+            flag: MazeFlag::Visited,
+        })
+    }
+}
--- src/player.rs
+++ src/player.rs
@@ -1,25 +1,53 @@
-use crate::maze::{Maze, MazeKind};
+use crate::maze::{Maze, MazeCell, MazeKind};
+use crate::maze::MazeKind::Start;
 
+#[derive(PartialEq, Debug)]
 struct Player {
-    width: u8,
-    height: u8,
+    width: usize,
+    height: usize,
 }
 
-impl Player {
-    fn init(width: u8, height: u8) -> Self {
-        Self { width, height }
-    }
+// impl Player {
+//     fn init(width: usize, height: usize) -> Self {
+//         Self { width, height }
+//     }
+//
+//     fn init_player(maze_width: usize, maze_height: usize, maze: Vec<Vec<Maze>>) -> Self {
+//         let mut player = Player { width: 0, height: 0 };
+//         for i in 0..maze_width
+//         {
+//             for j in 0..maze_height
+//             {
+//                 if MazeCell::compare(&maze[i as usize][j as usize].kind, &Start)
+//                 {
+//                     // player = Player::init(i, j);
+//                     break;
+//                 }
+//             }
+//         }
+//         player
+//     }
+// }
 
-    fn init_player(maze_width: u8, maze_height: u8, maze: Maze) -> Self {
-        let mut player = Player { width: 0, height: 0 };
-        for i in 0..maze_width {
-            for j in 0..maze_height {
-                if maze[i][j].kind == MazeKind::Start {
-                    player = Player::init(i, j);
-                }
-            }
-        }
-        player
-    }
-}
-
+// #[cfg(test)]
+// mod tests {
+//     use crate::maze::Maze;
+//     use crate::maze::MazeFlag::{Hide, Visited};
+//     use crate::maze::MazeKind::{Path, Start};
+//     use crate::player::Player;
+//
+//     #[test]
+//     fn should_create_player() {
+//         let maze_width = 5;
+//         let maze_height = 5;
+//         let maze = vec![vec![Maze { kind: Start, flag: Visited }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }],
+//                         vec![Maze { kind: Start, flag: Visited }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }],
+//                         vec![Maze { kind: Path, flag: Visited }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }],
+//                         vec![Maze { kind: Path, flag: Visited }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }],
+//                         vec![Maze { kind: Path, flag: Visited }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }], vec![Maze { kind: Path, flag: Hide }],
+//         ];
+//         let player = Player::init_player(maze_width, maze_height, maze);
+//         let test_player = Player::init(0, 0);
+//         assert_eq!(player, test_player)
+//     }
+// }
