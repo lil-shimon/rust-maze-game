81372446d8e9ee5226838b1d3cfdfe430abb4d5c
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-02-17 16:49:35 +0900

    test: 💍 added and pass create maze

--- src/maze.rs
+++ src/maze.rs
@@ -1,8 +1,8 @@
+use std::collections::BTreeMap;
+
 /*
  * 迷路の種類
  */
-use std::collections::BTreeMap;
-
 #[derive(PartialEq, Debug)]
 pub enum MazeKind {
     Path,
@@ -43,6 +43,7 @@ pub struct MazeRow {
     cells: Vec<MazeCell>,
 }
 
+#[derive(PartialEq, Debug)]
 pub struct Maze {
     width: usize,
     height: usize,
@@ -80,7 +81,7 @@ impl Maze {
 
 #[cfg(test)]
 mod test {
-    use crate::maze::{MazeCell, MazeFlag, MazeKind, MazeRow};
+    use crate::maze::{Maze, MazeCell, MazeFlag, MazeKind, MazeRow};
 
     #[test]
     fn should_create_maze_cell() {
@@ -105,6 +106,76 @@ mod test {
                         MazeCell { kind: MazeKind::Path, flag: MazeFlag::Hide },
                         MazeCell { kind: MazeKind::Path, flag: MazeFlag::Hide },
                         MazeCell { kind: MazeKind::Path, flag: MazeFlag::Hide })
+        })
+    }
+
+    #[test]
+    fn should_create_maze() {
+        let width = 5;
+        let height = 5;
+
+        let mut rows = vec![];
+        for i in 0..5
+        {
+            let mut cells: Vec<MazeCell> = vec![];
+            for i in 0..5
+            {
+                let cell = MazeCell::create(MazeKind::Path, MazeFlag::Visited);
+                cells.push(cell)
+            }
+            let row = MazeRow::create(cells);
+            rows.push(row)
+        }
+        assert_eq!(Maze::init(width, height, rows), Maze {
+            width: 5,
+            height: 5,
+            rows: vec!(
+                MazeRow {
+                    cells: vec!(
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                    )
+                },
+                MazeRow {
+                    cells: vec!(
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                    )
+                },
+                MazeRow {
+                    cells: vec!(
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                    )
+                },
+                MazeRow {
+                    cells: vec!(
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                    )
+                },
+                MazeRow {
+                    cells: vec!(
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                        MazeCell { kind: MazeKind::Path, flag: MazeFlag::Visited },
+                    )
+                },
+            ),
         })
     }
 }
